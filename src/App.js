772004735b7adde71ee895d383408d24badc90fa
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-17 20:28:57 -0500

    add test data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { useState } from "react";
+import { data } from "./data";
 
 function App() {
-  return <div className="App"></div>;
+  const [people, setPeople] = useState(data);
+  return (
+    <div className="App">
+      {people.map((item) => {
+        const { id, name } = item;
+        return (
+          <div key={id}>
+            <h2>
+              {id} - {name}
+            </h2>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const data = [
+  {
+    id: 1,
+    name: "Lily",
+  },
+  {
+    id: 2,
+    name: "Maria",
+  },
+  {
+    id: 3,
+    name: "Kate",
+  },
+];
