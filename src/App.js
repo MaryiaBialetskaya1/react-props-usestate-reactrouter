0c303f9dbea18620c2d81ac672445551b163a346
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-18 20:15:26 -0500

    start work on sipmle online store

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import { data, dataDogs } from "./data";
 import { People } from "./components/People";
 import { dataBirds } from "./data";
 import { Birds } from "./components/Birds";
-import { Dogs } from "./components/Gogs";
+import { Dogs } from "./components/Dogs";
 
 function App() {
-  const [people, setPeople] = useState(data);
-  const [birds, setBirds] = useState(dataBirds);
-  const [dogs, setDogs] = useState(dataDogs);
+  // const [people, setPeople] = useState(data);
+  // const [birds, setBirds] = useState(dataBirds);
+  // const [dogs, setDogs] = useState(dataDogs);
   return (
     <div>
-      <People propName={people} />
+      {/* <People propName={people} />
       <Birds birds={birds} />
-      <Dogs dogs={dogs} />
+      <Dogs dogs={dogs} /> */}
     </div>
   );
 }
