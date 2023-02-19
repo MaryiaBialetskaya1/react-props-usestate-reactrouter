a355586576db7d4b3091537a5d662851b402afce
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-18 20:39:39 -0500

    delete unused styles

--- src/components/SimpleOnlineStore/SimpleOnlineStore.js
+++ src/components/SimpleOnlineStore/SimpleOnlineStore.js
@@ -8,7 +8,7 @@ export function SimpleOnlineStore({ onlineStore }) {
         return (
           <div key={id} className={style.productCard}>
             <img src={image} alt="store items" width="400px" height="500px" />
-            <div className={style.}>
+            <div>
               <h3>{name}</h3>
               <h4> {searchTerm}</h4>
               <h4>$ {price}</h4>
