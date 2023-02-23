28f9f8d30594ff8395214e1391b7f5e28e8e6a19
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-23 13:43:55 -0500

    add hashrouter

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import "./App.css";
 import { People } from "./components/People/People";
 import { Birds } from "./components/Birds/Birds";
 import { Dogs } from "./components/Dogs/Dogs";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  HashRouter,
+} from "react-router-dom";
 import { Navbar } from "./components/Havbar/Navbar";
 import { Home } from "./components/Home/Home";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/dogs" element={<Dogs />} />
-          <Route path="/birds" element={<Birds />} />
-          <Route path="/people" element={<People />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <HashRouter basename="/">
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/dogs" element={<Dogs />} />
+            <Route path="/birds" element={<Birds />} />
+            <Route path="/people" element={<People />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </HashRouter>
     </Router>
   );
 }
